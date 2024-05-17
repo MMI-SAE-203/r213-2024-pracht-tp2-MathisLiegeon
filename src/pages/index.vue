d2d40baa6e161b5934d9bae88119714d389d2d97
W33dw3y <150056959+W33dw3y@users.noreply.github.com>
2024-05-17 10:44:50 +0200

    generation des types

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,15 +1,15 @@
 <!-- eslint-disable vue/multi-word-component-names -->
 <script setup lang="ts">
-import type { maisonRecord } from '@/types'
+import type { MaisonResponse } from '@/pocketbase-types'
 import MaisonCard from '@/components/MaisonCard.vue'
 import { allMaison, pb } from '@/backend'
-const maisonsListe: maisonRecord[] = await pb.collection('maison').getFullList()
+const maisonsListe: MaisonResponse[] = await pb.collection('maison').getFullList()
 console.log(maisonsListe)
 
 </script>
 
 <template>
   <h1 class="text-2xl">Bonjour monde !</h1>
-  
+
   <MaisonCard v-for="uneMaison in maisonsListe" :key="uneMaison.id" v-bind="uneMaison"/>
 </template>
--- src/pocketbase-types.ts
+++ src/pocketbase-types.ts
@@ -1,10 +1,77 @@
 /**
- * Générer ce fichier avec la commande suivante:
- * npm run typegen
- */
+* This file was @generated using pocketbase-typegen
+*/
 
 import type PocketBase from 'pocketbase'
+import type { RecordService } from 'pocketbase'
+
+export enum Collections {
+	Agent = "agent",
+	Maison = "maison",
+}
+
+// Alias types for improved usability
+export type IsoDateString = string
+export type RecordIdString = string
+export type HTMLString = string
+
+// System fields
+export type BaseSystemFields<T = never> = {
+	id: RecordIdString
+	created: IsoDateString
+	updated: IsoDateString
+	collectionId: string
+	collectionName: Collections
+	expand?: T
+}
+
+export type AuthSystemFields<T = never> = {
+	email: string
+	emailVisibility: boolean
+	username: string
+	verified: boolean
+} & BaseSystemFields<T>
+
+// Record types for each collection
+
+export type AgentRecord = {
+	eMail?: string
+	nom?: string
+	numTel?: string
+	prenom?: string
+}
+
+export type MaisonRecord = {
+	adresse?: string
+	favori?: boolean
+	images?: string[]
+	nbChambres?: number
+	nbSdb?: number
+	nomMaison?: string
+	prix?: number
+	surface?: number
+}
+
+// Response types include system fields and match responses from the PocketBase API
+export type AgentResponse<Texpand = unknown> = Required<AgentRecord> & BaseSystemFields<Texpand>
+export type MaisonResponse<Texpand = unknown> = Required<MaisonRecord> & BaseSystemFields<Texpand>
+
+// Types containing all Records and Responses, useful for creating typing helper functions
+
+export type CollectionRecords = {
+	agent: AgentRecord
+	maison: MaisonRecord
+}
+
+export type CollectionResponses = {
+	agent: AgentResponse
+	maison: MaisonResponse
+}
+
 // Type for usage with type asserted PocketBase instance
 // https://github.com/pocketbase/js-sdk#specify-typescript-definitions
 
-export type TypedPocketBase = PocketBase & {}
+export type TypedPocketBase = PocketBase & {
+	collection(idOrName: 'agent'): RecordService<AgentResponse>
+	collection(idOrName: 'maison'): RecordService<MaisonResponse>
+}
